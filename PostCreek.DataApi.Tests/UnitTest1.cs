1a580a4e50a654c9d805b4600111827461159e78
John Cote <postcreek@outlook.com>
2017-07-13 15:29:53 -0600

    project create

---
+++ PostCreek.DataApi.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PostCreek.DataApi.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PostCreek.DataApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PostCreek.DataApi.WebApiApplication" Language="C#" %>
